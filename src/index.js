4cef0c88efd87c2d1564e8433068b1b0b1dd887b
febalist <febalist@gmail.com>
2019-06-10 18:12:49 +0300

    Fix

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ export function install(Vue) {
   if (install.installed) return;
   install.installed = true;
 
-  Vue.component('model-media-edit', require('./components/model-media-edit'));
+  Vue.component('model-media-edit', require('./components/model-media-edit').default);
 }
